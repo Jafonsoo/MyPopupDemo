78fd316e794255fb5f72dba1ceb6090d67adbf49
joao.afonso <afonso.aires@hotmail.com>
2021-09-28 11:25:31 +0100

    Add project files.

---
+++ MyPopupDemo/MyPopupDemo/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Rg.Plugins.Popup.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MyPopupDemo
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await PopupNavigation.PushAsync(new ShowPopupDemo());
+        }
+        private async void Loader1_Clicked(object sender, EventArgs e)
+        {
+            var loadingPage = new LoadingPopupPage();
+            await PopupNavigation.PushAsync(loadingPage);
+            await Task.Delay(2000);
+            await PopupNavigation.RemovePageAsync(loadingPage);
+        }
+        private async void Loader2_Clicked(object sender, EventArgs e)
+        {
+            var loadingPage = new CustomLoaderPage();
+            await PopupNavigation.PushAsync(loadingPage);
+            await Task.Delay(2000);
+            await PopupNavigation.RemovePageAsync(loadingPage);
+        }
+        private async void Loader3_Clicked(object sender, EventArgs e)
+        {
+            var loadingPage = new RandomColorLoader();
+            await PopupNavigation.PushAsync(loadingPage);
+            await Task.Delay(6000);
+            await PopupNavigation.RemovePageAsync(loadingPage);
+        }
+        private async void GIF_Clicked(object sender, EventArgs e)
+        {
+            var loadingPage = new CustomGIFLoader();
+            await PopupNavigation.PushAsync(loadingPage);
+            await Task.Delay(6000);
+            await PopupNavigation.RemovePageAsync(loadingPage);
+        }
+    }
+}
